a2613a124af6abff3b4ad04a177d13396f82bb12
Prateek_Upadhyay <prateek.upadhyay@mail.vinove.com>
2023-09-06 17:32:41 +0530

    Redux implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
-import Constants from './Helpers';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import RoutesWrapper from './Router';
-// import { persistor, store } from './Redux/store';
-import './App.css';
-
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import RoutesWrapper from "./Router";
+import store from "./Redux/store";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Provider store={store}> */}
-            <ToastContainer />
-            <BrowserRouter>
-              <RoutesWrapper />
-            </BrowserRouter>
-      {/* </Provider> */}
-    </div>
+    <Provider store={store}>
+      <ToastContainer />
+      <BrowserRouter>
+        <RoutesWrapper />
+      </BrowserRouter>
+    </Provider>
   );
 }
 
