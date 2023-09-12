6bbc91669721575615e5c2126e03858611af546c
dhruv0724 <dhruv.verma@mail.vinove.com>
2023-09-12 16:27:13 +0530

    12-09-2023 side options

--- src/Components/w9/W9Forms/tabMenu.tsx
+++ src/Components/w9/W9Forms/tabMenu.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 export default function Tab(){
     return(
-        <div>
-            113
+        <div className="overlay-div">
+            <div className="overlay-div-group">
+                <div className="viewInstructions">View Instructions</div>
+                <div className="viewform">View Form</div>
+                <div className="helpvideo"> <a target="new" href="https://youtu.be/SqcY0GlETPk?si=KOwsaYzweOessHw-">Help Video</a></div>
+            </div>
         </div>
     )
 }
