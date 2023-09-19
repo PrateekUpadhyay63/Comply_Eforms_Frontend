ac8c7c2da876670e4ce830d8a5898aa06506b466
PrateekUpadhyay63 <pratee.upadhyay@mail.vinove.com>
2023-09-19 10:01:05 +0530

    Tab component onclick class

--- src/Components/w9/W9Forms/tabMenu.tsx
+++ src/Components/w9/W9Forms/tabMenu.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 export default function Tab(){
+    const [click,setClick]=useState(false);
     return(
         <div className="overlay-div">
+             <div className={click ? "abc" :"xyz"}>
+          XYZ
+        </div>
             <div className="overlay-div-group">
                 <div className="viewInstructions">View Instructions</div>
-                <div className="viewform">View Form</div>
+                <div className="viewform" onClick={()=>setClick(!click)}>View Form</div>
                 <div className="helpvideo"> 
                 {/* <a target="_blank" href="https://youtu.be/SqcY0GlETPk?si=KOwsaYzweOessHw-">Help Video</a> */}
                 <a href="https://youtu.be/SqcY0GlETPk?si=KOwsaYzweOessHw-" target="popup" onClick={()=>window.open('https://youtu.be/SqcY0GlETPk?si=KOwsaYzweOessHw-','name','width=600,height=400')}>Help Video</a>
