66c5a2ae687d5752d4863c35e79c41697be0134d
PrateekUpadhyay63 <pratee.upadhyay@mail.vinove.com>
2023-10-06 12:48:30 +0530

    .

--- src/Components/individualUS/index.tsx
+++ src/Components/individualUS/index.tsx
@@ -154,7 +154,7 @@ export default function IndividualUs() {
     foreignTINCountryId: 0,
     foreignTIN: "",
     foreignTINNotAvailable: false, //
-    alternativeTINFormat: "yes", //
+    alternativeTINFormat: true, //
     giin: "",
     permanentResidentialCountryId: 0,
     permanentResidentialStreetNumberandName: "",
@@ -459,17 +459,16 @@ export default function IndividualUs() {
     }
   };
 
-  const formatTin = (e: any) => {
+  const formatTin = (e: any,values:any):any => {
     if (e.key === "Backspace" || e.key === "Delete") return;
     if (e.target.value.length === 3) {
       setPayload({ ...payload, usTin: payload.usTin + "-" });
+      values.usTin= values.usTin+"-"
     }
     if (e.target.value.length === 6) {
       setPayload({ ...payload, usTin: payload.usTin + "-" });
+      values.usTin= values.usTin+"-"
     }
-    // if(e.target.value.length === 14) {
-    //   setPayload({ ...payload, usTin: payload.usTin +"-" })
-    // }
   };
   // const clickInfo = () => {
   //   alert(
@@ -573,8 +572,7 @@ export default function IndividualUs() {
                   foreignTINCountryId: values?.foreignTINCountryId,
                   foreignTIN: values?.foreignTIN,
                   foreignTINNotAvailable: values?.foreignTINNotAvailable, //
-                  alternativeTINFormat:
-                    values?.alternativeTINFormat == "yes" ? true : false, //
+                  alternativeTINFormat: values?.alternativeTINFormat, //
                   giin: values?.giin,
                   permanentResidentialCountryId:
                     values?.permanentResidentialCountryId,
@@ -1407,9 +1405,9 @@ export default function IndividualUs() {
                                 id="outlined"
                                 name="usTin"
                                 placeholder="Enter U.S. TIN"
-                                onKeyDown={formatTin}
+                                onKeyDown={(e)=>formatTin(e,values)}
                                 onChange={handleChange}
-                                inputProps={{ maxLength: 11 }}
+                                inputProps={{ maxLength: 9 }}
                                 // onBlur={handleBlur}
                                 //   error={Boolean(touched.usTin && errors.usTin)}
                                 value={values.usTin}
@@ -1531,7 +1529,7 @@ export default function IndividualUs() {
                                 inputProps={{ "aria-label": "Yes" }}
                               />
                             </div> */}
-                            {/* <div className="d-flex">
+                            <div className="d-flex">
                               <Typography
                                 style={{ fontSize: "13px", marginTop: "10px" }}
                               >
@@ -1612,45 +1610,6 @@ export default function IndividualUs() {
                                   ""
                                 )}
                               </FormControl>
-                            </div> */}
-                            <div className="d-flex">
-                              <FormControl
-                                error={Boolean(
-                                  touched.alternativeTINFormat &&
-                                    errors.alternativeTINFormat
-                                )}
-                              >
-                                <RadioGroup
-                                  id="alternativeTINFormat"
-                                  row
-                                  aria-labelledby="demo-row-radio-buttons-group-label"
-                                  value={values.alternativeTINFormat}
-                                  onChange={handleChange}
-                                >
-                                  <FormControlLabel
-                                    control={<Radio />}
-                                    value="yes"
-                                    name="alternativeTINFormat"
-                                    label="Not Available"
-                                  />
-                                  <FormControlLabel
-                                    control={<Radio />}
-                                    value="no"
-                                    name="alternativeTINFormat"
-                                    label="Alternative TIN Format"
-                                  />
-                                </RadioGroup>
-                                {errors.alternativeTINFormat &&
-                                touched.alternativeTINFormat ? (
-                                  <div>
-                                    <Typography color="error">
-                                      {errors.alternativeTINFormat}
-                                    </Typography>
-                                  </div>
-                                ) : (
-                                  ""
-                                )}
-                              </FormControl>
                             </div>
                           </div>
                           <div className="col-12 d-flex">
@@ -1709,7 +1668,7 @@ export default function IndividualUs() {
                                   placeholder="Enter Value Added Tax Number"
                                   // onKeyDown={formatTin}
                                   onChange={handleChange}
-                                  inputProps={{ maxLength: 11 }}
+                                  inputProps={{ maxLength: 9 }}
                                   // onBlur={handleBlur}
                                   //   error={Boolean(touched.usTin && errors.vat)}
                                   value={values.vat}
@@ -1771,7 +1730,7 @@ export default function IndividualUs() {
                                 id="outlined"
                                 name="usTin"
                                 placeholder="Enter U.S. TIN"
-                                onKeyDown={formatTin}
+                                onKeyDown={(e)=>formatTin(e,values)}
                                 onChange={handleChange}
                                 inputProps={{ maxLength: 11 }}
                                 // onBlur={handleBlur}
@@ -4116,7 +4075,7 @@ export default function IndividualUs() {
                                         touched.accountHolderName &&
                                           errors.accountHolderName
                                       )}
-                                      value={values.accountHolderName}
+                                      value={values.accountHolderName === "" ? values.firstName+ " "+values.lastName :values.accountHolderName}
                                     />
                                     <p className="error">
                                       {errors.accountHolderName}
@@ -4871,7 +4830,7 @@ export default function IndividualUs() {
                       <div className="text-center">
                         <Button
                           type="submit"
-                          disabled={isSubmitting}
+                          disabled={!values.isConfirmed}
                           // onClick={() => history("/Term")}
                           style={{
                             border: "1px solid #0095dd",
